f3c37182a441feedb9c5e5a0edee1f9e1b75a2c3
Toni Saarela <tn.saarela.dev@gmail.com>
2016-12-14 16:11:52 +0100

    add view controllers and setup launch screen

--- mobileApp/AccountViewController.h
+++ mobileApp/AccountViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  AccountViewController.h
 //  mobileApp
 //
 //  Created by admin_user on 12/14/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface AccountViewController : UIViewController
 
 @end
-
---
+++ mobileApp/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  mobileApp
+//
+//  Created by admin_user on 12/14/16.
+//  Copyright © 2016 ServersCheck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
