765ca67df97533d52a9cfedff0c5a540ee98c30f
Toni Saarela <tn.saarela.dev@gmail.com>
2016-12-14 14:28:32 +0100

    initial commit

---
+++ mobileApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mobileApp
+//
+//  Created by admin_user on 12/14/16.
+//  Copyright © 2016 ServersCheck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mobileApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mobileApp
+//
+//  Created by admin_user on 12/14/16.
+//  Copyright © 2016 ServersCheck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
